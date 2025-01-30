66774a5efcd4bc1aa795c4d7c2f66bf219225bbe
otabek-a <otabekabdurasulov245@gmail.com>
2025-01-30 23:07:14 +0500

    save emojies in tinydb

--- main.py
+++ main.py
@@ -1,54 +1,67 @@
-from telegram.ext import Updater,MessageHandler,Filters,CommandHandler,CallbackContext,Filters, CallbackQueryHandler
+from telegram.ext import Updater,MessageHandler,Filters,CommandHandler,CallbackContext, CallbackQueryHandler
 from telegram import Bot, Update,InlineKeyboardButton,InlineKeyboardMarkup
 import os
-from tinydb import TinyDB
-emoji=TinyDB('emoji.json')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def info(update: Update, context):
-    photo=update.message.photo[-1]
-    file_id=photo.file_id
-    
-    update.message.reply_photo(photo=file_id,caption='hey bro , for add emojies send me them')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from tinydb import TinyDB,Query
+User=Query()
+db=TinyDB('db.json')
 def start(update: Update, context):
     update.message.reply_text("Hey please send photo for work this bot")
 
-TOKEN = '7554317270:AAHn7TeDzbxCl3LJ3XXOKPnjMK8oZ-R53Jc'
+
+
+
+
+    
+def info(update: Update, context):
+    photo=update.message.photo[-1]
+    file_id=photo.file_id
+    emojis = db.all()
+    if emojis:
+       buttons=[
+           [InlineKeyboardButton(text=f"{emoji['emoji']} ({emoji['count']})",callback_data=f"{i}")]
+           for i,emoji in enumerate(emojis)]
+       inline_keyboard = InlineKeyboardMarkup(buttons)
+       update.message.reply_photo(photo=file_id, caption='If you want to add more emoji please send it.', reply_markup=inline_keyboard)
+    else:
+        update.message.reply_photo(photo=file_id, caption='I am so sorry , you dont have any emojies.')
+
+       
+
+   
+
+def add_emoji(update, context):
+    db.insert({"emoji": update.message.text,'count':0})
+    update.message.reply_text("Good job i added your emoji to your list")
+
+
+
+def button_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    index = int(query.data)
+    emojis = db.all()
+    matn=emojis[index]
+    matn['count']+=1
+    db.update({'count':matn['count']},User.emoji==matn['emoji'])
+    buttons = [
+        [InlineKeyboardButton(
+            text=f"{emoji['emoji']} ({emoji['count']})", 
+            callback_data=f"{i}"
+        )]
+        for i, emoji in enumerate(emojis)
+    ]
+    inline_keyboard = InlineKeyboardMarkup(buttons)
+    query.edit_message_reply_markup(reply_markup=inline_keyboard)
+
+
+    
+
+
+
+
+
+    
+
+TOKEN = os.environ['TOKEN']
 
 updater = Updater(token=TOKEN)
 
@@ -57,10 +70,10 @@ dispatcher = updater.dispatcher
 # add handlers here
 
 
-
-dispatcher.add_handler(MessageHandler(Filters.photo, info))
-
 dispatcher.add_handler(CommandHandler('start',start))
+dispatcher.add_handler(MessageHandler(Filters.text, add_emoji))
+dispatcher.add_handler(MessageHandler(Filters.photo, info))
+dispatcher.add_handler(CallbackQueryHandler(button_callback))
 
 updater.start_polling()
 updater.idle()
